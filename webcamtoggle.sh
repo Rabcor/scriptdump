26b903c91ccbe6d62725b0ddf10bc51643a1f41f
Rabcor <rabcor1@hotmail.com>
2021-10-02 20:06:40 +0000

    Update webcamtoggle.sh

--- webcamtoggle.sh
+++ webcamtoggle.sh
@@ -4,7 +4,7 @@ CONFIG=$( grep $ID /sys/bus/usb/devices/*/idProduct | sed s/idProduct:$ID//g )bC
 
 if ( lsmod | grep uvcvideo &> /dev/null ); then
     pkexec sh -c 'echo 0 > '$CONFIG'; sleep 2; rmmod -f uvcvideo'
-	notify-send -a "Webcam" "Webcam off"
+    notify-send -a "Webcam" "Webcam off"
 else
     pkexec sh -c 'echo 1 > '$CONFIG'; modprobe uvcvideo'
     notify-send -a "Webcam" "Webcam on"
