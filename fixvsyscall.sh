880affa81b6fa688ac09068be1a7dd7541286e72
Rabcor <rabcor1@hotmail.com>
2021-10-03 01:30:34 +0000

    Add files via upload

---
+++ fixvsyscall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Some anti-cheat software (notably in League of Legends) needs abi.vsyscall32 to be disabled. This script re-enables it, and is meant to be used as a post-exit script.
+
+STATE=$(sysctl -n abi.vsyscall32)
+EXEC=kdesu # Options: pkexec, kdesu
+
+if [ "$STATE" = "0" ]; then
+    $EXEC sh -c 'sysctl -w abi.vsyscall32=1'
+    echo "abi.vsyscall32 = $STATE"
+fi
+
